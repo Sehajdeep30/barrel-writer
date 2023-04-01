42b7df48a033319f8f44f4b25c9c02842bcfdfc6
s-m33r <>
2023-04-02 04:13:33 +0530

    CORS issue resolved

--- api/server.py
+++ api/server.py
@@ -1,9 +1,21 @@
 import sys
 import os
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# allow cors
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load the model
 if "models" not in os.listdir():
@@ -15,15 +27,26 @@ scifi_model = GPT2LMHeadModel.from_pretrained("models/scifi-speculative")
 print("scifi model loaded")
 philosophy_model = GPT2LMHeadModel.from_pretrained("models/philosophy")
 print("philosophy model loaded")
+academia_model = GPT2LMHeadModel.from_pretrained("models/academia")
+print("academia model loaded")
+litreature_model = GPT2LMHeadModel.from_pretrained("models/litreature")
+print("litreature model loaded")
 
-# Load the pre-trained GPT2 tokenizer 
+# Load the pre-trained GPT2 tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-OUTPUT_LENGTH = 50
-
 # Generate text from a prompt
-@app.get("/{model}/{length}/{prompt}")
-async def generate(model: str, length: int, prompt: str) -> dict:
+@app.post("/")
+async def generate(request: Request):
+    req = await request.json()
+
+    print(req)
+
+    length = req["length"]
+    model = req["model"]
+    prompt = req["prompt"]
+
+    
     if length > 100:
         length = 100
     if length < 0 or length < 10:
@@ -31,12 +54,22 @@ async def generate(model: str, length: int, prompt: str) -> dict:
 
     input_ids = tokenizer.encode(prompt, return_tensors='pt')
 
+    generated_text = ""
+
     if model == "scifi":
         output = scifi_model.generate(input_ids, max_length=length, do_sample=True, pad_token_id=tokenizer.eos_token_id)
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
     elif model == "philosophy":
-        output = philosophy_model.generate(input_ids, max_length=OUTPUT_LENGTH, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+        output = philosophy_model.generate(input_ids, max_length=length, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+        generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    elif model == "academia":
+        output = academia_model.generate(input_ids, max_length=length, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+        generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    elif model == "litreature":
+        output = litreature_model.generate(input_ids, max_length=length, do_sample=True, pad_token_id=tokenizer.eos_token_id)
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
     return {
